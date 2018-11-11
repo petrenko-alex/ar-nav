72d66e99a6ce77710994e6126a4cdd3322657012
Alexander Petrenko <gafk555@gmail.com>
2018-11-11 20:48:28 +0400

    add stubs for places

---
+++ src/stubs/places.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    name: 'Кафедра ПОАС',
+    description: 'Этаж кафедры ПОАС. Учебно-лабораторный корпус "В". 9 этаж.',
+    image: 'https://picsum.photos/200?random'
+  },
+  {
+    id: 2,
+    name: 'Кафедра ЭВМ',
+    description: 'Этаж кафедры ЭВМ. Учебно-лабораторный корпус "В". 12 этаж.',
+    image: 'https://picsum.photos/200?random'
+  },
+  {
+    id: 3,
+    name: 'Кафедра САПР',
+    description: 'Этаж кафедры САПРиПК. Учебно-лабораторный корпус "В". 14 этаж.',
+    image: 'https://picsum.photos/200?random'
+  }
+]
