6b49a07cfc15c58d6a2aeb9c387da3dee4e73895
Alexander Petrenko <gafk555@gmail.com>
2018-11-11 18:01:29 +0400

    add placeitem component

---
+++ src/components/PlaceItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: "PlaceItem",
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
