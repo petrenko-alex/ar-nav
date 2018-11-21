bd13b7c6ef1dc9a2ecdee5a3de5de30cb148e695
Alexander Petrenko <gafk555@gmail.com>
2018-11-21 20:32:31 +0400

    add genuinely random image for stubs

--- src/stubs/places.js
+++ src/stubs/places.js
@@ -3,18 +3,18 @@ export default [
     id: 1,
     name: 'Кафедра ПОАС',
     description: 'Этаж кафедры ПОАС. Учебно-лабораторный корпус "В". 9 этаж.',
-    image: 'https://picsum.photos/200?random'
+    image: 'https://source.unsplash.com/random/200x200?sig=' + Math.round(Math.random() * 1000)
   },
   {
     id: 2,
     name: 'Кафедра ЭВМ',
     description: 'Этаж кафедры ЭВМ. Учебно-лабораторный корпус "В". 12 этаж.',
-    image: 'https://picsum.photos/200?random'
+    image: 'https://source.unsplash.com/random/200x200?sig=' + Math.round(Math.random() * 1000)
   },
   {
     id: 3,
     name: 'Кафедра САПР',
     description: 'Этаж кафедры САПРиПК. Учебно-лабораторный корпус "В". 14 этаж.',
-    image: 'https://picsum.photos/200?random'
+    image: 'https://source.unsplash.com/random/200x200?sig=' + Math.round(Math.random() * 1000)
   }
 ]
