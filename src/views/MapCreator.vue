892a93d16c77ebb5d8948c59091ddb7230a68088
Alexander Petrenko <gafk555@gmail.com>
2019-01-15 22:01:30 +0400

    add MapCreator view

---
+++ src/views/MapCreator.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-content>
+        <div class="ma-5 text-xs-center headline">
+            Раздел находится в разработке...
+        </div>
+    </v-content>
+</template>
+
+<script>
+  export default {
+    name: "MapCreator"
+  }
+</script>
