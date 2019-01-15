6e1dd904066179fc9b17a5809df3a993c3be3325
Alexander Petrenko <gafk555@gmail.com>
2019-01-15 16:43:50 +0400

    exclude navigation and toolbar from Home view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <div id="app">
-    <router-view/>
+    <v-app>
+      <v-navigation-drawer fixed app v-model="showSideMenu">
+        <v-list dense>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Домой</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-toolbar app>
+        <v-toolbar-side-icon @click.stop="showSideMenu = !showSideMenu"></v-toolbar-side-icon>
+        <v-toolbar-title class="headline text-uppercase">
+          <span>{{ appName }}</span>
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <router-view/>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      showSideMenu: false,
+      appName: 'ar-nav',
+      routes: this.$router.options.routes,
+    }
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,7 @@
 <template>
-  <v-app>
-    <v-navigation-drawer fixed app v-model="showSideMenu">
-      <v-list dense>
-          <v-list-tile>
-              <v-list-tile-action>
-                  <v-icon>home</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                  <v-list-tile-title>Домой</v-list-tile-title>
-              </v-list-tile-content>
-          </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="showSideMenu = !showSideMenu"></v-toolbar-side-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>{{ appName }}</span>
-      </v-toolbar-title>
-    </v-toolbar>
-
     <v-content>
       <PlaceList></PlaceList>
     </v-content>
-  </v-app>
 </template>
 
 <script>
@@ -33,12 +12,5 @@
       components: {
           PlaceList
       },
-      data() {
-          return {
-            appName: 'ar-nav',
-            showSideMenu: false,
-            routes: this.$router.options.routes,
-          }
-      }
   }
 </script>
