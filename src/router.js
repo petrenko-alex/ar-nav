42ce6c2f9ee184ec9a39ae9093da7bf1dd7dc8fe
Alexander Petrenko <gafk555@gmail.com>
2019-01-15 22:02:36 +0400

    update router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import MapCreator from './views/MapCreator'
 
 Vue.use(Router);
 
@@ -13,6 +14,15 @@ export default new Router({
       meta: {
         icon: 'home',
         title: 'Домой',
+      }
+    },
+    {
+      path: '/map-creator',
+      name: 'map_creator',
+      component: MapCreator,
+      meta: {
+        icon: 'fas fa-map-marked-alt',
+        title: 'Генератор карт',
       }
     },
   ]
