47392b3589295946db414f74ea2fa3eed614fa2d
Alexander Petrenko <gafk555@gmail.com>
2019-02-02 15:30:27 +0400

    add ArRoom view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import MapCreator from './views/MapCreator'
+import ArRoom from './views/ArRoom'
 
 Vue.use(Router);
 
@@ -25,6 +26,14 @@ export default new Router({
         showInMenu: true,
         icon: 'fas fa-map-marked-alt',
         title: 'Генератор карт',
+      }
+    },
+    {
+      path: '/ar-room/:id',
+      name: 'ar_room',
+      component: ArRoom,
+      meta: {
+        showInMenu: false,
       }
     },
   ]
---
+++ src/views/ArRoom.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-content>
+        <div class="ma-5 text-xs-center headline">
+            {{ $route.params.id }}
+        </div>
+    </v-content>
+</template>
+
+<script>
+  export default {
+    name: "ArRoom"
+  }
+</script>
