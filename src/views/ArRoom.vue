0ee409ad3f7361960b2d498786000e365befe229
Alexander Petrenko <gafk555@gmail.com>
2019-02-02 19:11:05 +0400

    add ar.js template

--- src/views/ArRoom.vue
+++ src/views/ArRoom.vue
@@ -1,14 +1,15 @@
 <template>
     <v-content>
-        <div class="ma-5 text-xs-center headline">
-            {{ id }}
-        </div>
+        <a-scene embedded artoolkit="sourceType: webcam;">
+            <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+            <a-marker-camera preset='hiro'></a-marker-camera>
+        </a-scene>
     </v-content>
 </template>
 
 <script>
   export default {
     name: "ArRoom",
-    props: ['id']
+    props: ['id'],
   }
 </script>
