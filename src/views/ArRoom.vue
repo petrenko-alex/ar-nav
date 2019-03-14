4749d63570ded35c2886753dd1126216f5506124
Alexander Petrenko <gafk555@gmail.com>
2019-03-14 18:16:57 +0400

    make ArRoom decode qrcode and show marker info

--- src/views/ArRoom.vue
+++ src/views/ArRoom.vue
@@ -1,16 +1,76 @@
 <template>
     <v-content>
+        <qrcode-stream :track="false" @decode="onDecode" @init="onInit"/>
         <a-scene embedded artoolkit="sourceType: webcam;">
-            <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+            <a-box position='0 0.5 -1' material='opacity: 0.5;'></a-box>
             <a-marker-camera preset='hiro'></a-marker-camera>
         </a-scene>
     </v-content>
 </template>
 
 <script>
+  import {QrcodeStream, QrcodeDropZone, QrcodeCapture} from 'vue-qrcode-reader'
+
   export default {
     name: "ArRoom",
     props: ['id'],
+    components: {
+      QrcodeStream,
+    },
+    data() {
+      return {
+        markersUrl: 'markers/getmarkersforplace.php',
+        roomId: 0,
+        markers: {},
+      }
+    },
+    created() {
+      this.roomId = this.$route.params['id'];
+
+      this.$http.get(
+        this.$root.baseApiUrl + this.markersUrl,
+        {params: {placeId: this.roomId, XDEBUG_SESSION_START:'PHPSTORM'}}
+      )
+        .then(
+          response => {
+            if (response.ok) {
+              this.markers = response.body;
+            } else {
+              console.log('Error getting markers for place. Status text: ' + response.statusText);
+            }
+          },
+          response => {
+            console.log('Error getting markers for place. Status text: ' + response.statusText);
+          })
+    },
+    methods: {
+      onDecode(result) {
+        const markerId = +result;
+        if(this.markers.hasOwnProperty(markerId)) {
+          console.log(this.markers[markerId].info);
+        }
+      },
+
+      async onInit(promise) {
+        try {
+          await promise
+        } catch (error) {
+          if (error.name === 'NotAllowedError') {
+            console.log('ERROR: you need to grant camera access permisson');
+          } else if (error.name === 'NotFoundError') {
+            console.log('ERROR: no camera on this device');
+          } else if (error.name === 'NotSupportedError') {
+            console.log('ERROR: secure context required (HTTPS, localhost)');
+          } else if (error.name === 'NotReadableError') {
+            console.log('ERROR: is the camera already in use?');
+          } else if (error.name === 'OverconstrainedError') {
+            console.log('ERROR: installed cameras are not suitable');
+          } else if (error.name === 'StreamApiNotSupportedError') {
+            console.log('ERROR: Stream API is not supported in this browser');
+          }
+        }
+      }
+    }
   }
 </script>
 
