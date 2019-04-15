cba0c42f0722202244a7af1dd8b6426141016be1
Alexander Petrenko <gafk555@gmail.com>
2019-04-15 22:41:05 +0400

    edit baseApiUrl for prod server

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ new Vue({
   router,
   Resource,
   data: {
-    baseApiUrl: 'https://ar-nav.ru/api/',
+    baseApiUrl: 'https://petrenko-story.ru/api/',
   },
   render: h => h(App)
 }).$mount('#app');
