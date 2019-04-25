524f5ec639d197b5815807490f3b6266a42a2e36
Alexander Petrenko <gafk555@gmail.com>
2019-04-25 23:51:08 +0400

    add MarkerInfo component

---
+++ src/components/MarkerInfo.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-dialog id="modal" v-model="show" width="500">
+        <v-card>
+            <v-card-title class="headline grey lighten-2" primary-title v-if="!!cardTitle">
+                {{ cardTitle }}
+            </v-card-title>
+            <v-card-text>
+               <slot></slot>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" flat @click.stop="show = false">
+                    <v-icon class="text-main-color">fas fa-times</v-icon>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: "MarkerInfo",
+    props: [
+      'card-title',
+      'value',
+    ],
+    computed: {
+      show: {
+        get() {
+         return this.value;
+        },
+        set(value) {
+          this.$emit('input', value);
+        }
+      }
+    }
+  }
+</script>
